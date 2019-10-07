2ca4f384c6deb35120643e1722f9dadfb1149819
bash4219 <kevin.bash18@gmail.com>
2019-10-07 15:36:36 -0500

    joke /WE end1-12

--- 1.py
+++ 1.py
@@ -13,11 +13,33 @@ print(y)
 print("I said: %r.:" % x)
 print("I alos said: '%s'." % y)
 
-hilarious = true
-jokeEvaluation = "ins't that joke so funny?! &r"
+hilarious = True
+jokeEvaluation = "ins't that joke so funny?! %r"
 
 print(jokeEvaluation % hilarious)
 
 W = "this is the left side of..."
 e = "a string with a rith side"
+print(W+e)
 
+# more printing fun
+print("mary had a little lamb.")
+print("its fleece was white as %s." % 'snow')
+print("and everywhere that mary went.")
+print("." * 10)
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+print(end1 + end2 + end3 + end4 + end5 +end6)
+print(end7 + end8 + end9 + end10 + end11 +end12)
