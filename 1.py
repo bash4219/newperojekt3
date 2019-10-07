beb8bdc62cd946009ae804ff8fabe89f2766b1ad
bash4219 <kevin.bash18@gmail.com>
2019-10-07 15:24:24 -0500

    joke

---
+++ 1.py
@@ -0,0 +1,17 @@
+# more strings and text
+
+
+x = "there are %d types fo people." %10
+binary = "binary"
+donot = "don't"
+y = "those who know %s and those who %s " % (binary, donot)
+
+
+print(x)
+print(y)
+
+print("I said: %r.:" % x)
+print("I alos said: '%s'." % y)
+
+hilarious = true
+jokeEvaluation = "ins't that joke so funny?!"
