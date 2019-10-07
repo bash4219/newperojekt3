0c21c1d8e16a540a47e205a1dcc2088d902acb70
bash4219 <kevin.bash18@gmail.com>
2019-10-07 15:26:37 -0500

    joke /WE

--- 1.py
+++ 1.py
@@ -14,4 +14,10 @@ print("I said: %r.:" % x)
 print("I alos said: '%s'." % y)
 
 hilarious = true
-jokeEvaluation = "ins't that joke so funny?!"
+jokeEvaluation = "ins't that joke so funny?! &r"
+
+print(jokeEvaluation % hilarious)
+
+W = "this is the left side of..."
+e = "a string with a rith side"
+
